ad3fed75d03f5eecf1adf300f170a6db0e1701cd
ssss <sdevdariani9@gmail.com>
2023-04-26 17:16:55 +0400

    Update models.py

--- models.py
+++ models.py
@@ -138,6 +138,8 @@ class Generator(nn.Module):
         self.feat_64  = UpBlock(nfc[32], nfc[64])
         self.feat_128 = UpBlockComp(nfc[64], nfc[128])  
         self.feat_256 = UpBlock(nfc[128], nfc[256]) 
+        self.feat_512 = UpBlockComp(nfc[256], nfc[512]) 
+        self.feat_1024 = UpBlock(nfc[512], nfc[1024])  
 
 
 
